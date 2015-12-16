3a3185ed1f6cde41d3c8e46660a4f7ba0869d1cc
Youngjun Kim <neosado@gmail.com>
2015-12-15 18:29:57 -0800

    update

--- simMineField.jl
+++ simMineField.jl
@@ -17,6 +17,7 @@ using Util
 
 using Distributions
 using Base.Collections
+using JLD
 
 
 #
@@ -302,62 +303,82 @@ end
 
 function runExpBatch(; bParallel::Bool = false)
 
-    N = 100
-
     nx = 7
     ny = 5
 
     srand(12)
-
-    reward_seed = rand(10000:typemax(Int16))
-    mf_seed_list = unique(rand(10000:typemax(Int16), round(Int64, N * 1.1)))[1:N]
-    mcts_seed_list = unique(rand(10000:typemax(Int16), round(Int64, N * 1.1)))[1:N]
+    nScenarios = 30
 
     tree_policies = Dict{ASCIIString, Any}[Dict("type" => :UCB1, "c" => 1.), Dict("type" => :UCB1, "c" => 300.), Dict("type" => :TS), Dict("type" => :TSM, "ARM" => () -> ArmRewardModel(0.01, 0.01, -50., 1., 1 / 2, 1 / (2 * (1 / abs(5) ^ 2)), -500., -1000., 1., 1 / 2, 0.001)), Dict("type" => :AUCB, "SP" => [Dict("type" => :UCB1, "c" => 1.), Dict("type" => :UCB1, "c" => 300.)])]
 
-    D = Dict{Dict{ASCIIString, Any}, Dict{ASCIIString, Any}}()
+    N = 100
 
-    if bParallel
-        if true
-            for tree_policy in tree_policies
-                results = pmap(id -> runExp(reward_seed, mf_seed_list[id], mcts_seed_list[id], tree_policy, 1, nx = nx, ny = ny, nloop_max = 1000000, nloop_min = 100, bParallel = true, id = id), 1:N)
 
-                opt_dist = 0
-                expected_returns = zeros(N)
+    scenarios = unique(rand(10000:typemax(Int16), round(Int64, nScenarios * 1.1)))[1:nScenarios]
 
-                for result in results
-                    id = result[1]
-                    opt_dist = result[2]
-                    expected_returns[id] = result[3]
+    for scenario in scenarios
+        println("Scenario: ", scenario)
+
+        srand(scenario)
+
+        mf_seed_list = unique(rand(10000:typemax(Int16), round(Int64, N * 1.1)))[1:N]
+        mcts_seed_list = unique(rand(10000:typemax(Int16), round(Int64, N * 1.1)))[1:N]
+
+        D = Dict{Tuple{Int64, Dict{ASCIIString, Any}}, Dict{ASCIIString, Any}}()
+
+        if bParallel
+            if true
+                for tree_policy in tree_policies
+                    results = pmap(id -> runExp(scenario, mf_seed_list[id], mcts_seed_list[id], tree_policy, 1, nx = nx, ny = ny, nloop_max = 1000000, nloop_min = 100, bParallel = true, id = id), 1:N)
+
+                    opt_dist = 0
+                    expected_returns = zeros(N)
+
+                    for result in results
+                        id = result[1]
+                        opt_dist = result[2]
+                        expected_returns[id] = result[3]
+                    end
+
+                    D[(scenario, tree_policy)] = Dict("mf_seed_list" => mf_seed_list, "mcts_seed_list" => mcts_seed_list, "N" => N, "nx" => nx, "ny" => ny, "opt_dist" => opt_dist, "expected_returns" => expected_returns)
                 end
 
-                D[tree_policy] = Dict("reward_seed" => reward_seed, "mf_seed_list" => mf_seed_list, "mcts_seed_list" => mcts_seed_list, "N" => N, "nx" => nx, "ny" => ny, "opt_dist" => opt_dist, "expected_returns" => expected_returns)
+            else
+                results = pmap(tree_policy -> runExp(scenario, mf_seed_list, mcts_seed_list, tree_policy, N, nx = nx, ny = ny, nloop_max = 1000000, nloop_min = 100, bParallel = true, id = tree_policy), tree_policies)
+
+                for result in results
+                    tree_policy = result[1]
+                    opt_dist = result[2]
+                    expected_returns = result[3]
+
+                    D[(scenario, tree_policy)] = Dict("mf_seed_list" => mf_seed_list, "mcts_seed_list" => mcts_seed_list, "N" => N, "nx" => nx, "ny" => ny, "opt_dist" => opt_dist, "expected_returns" => expected_returns)
+                end
+
             end
 
         else
-            results = pmap(tree_policy -> runExp(reward_seed, mf_seed_list, mcts_seed_list, tree_policy, N, nx = nx, ny = ny, nloop_max = 1000000, nloop_min = 100, bParallel = true, id = tree_policy), tree_policies)
-
-            for result in results
-                tree_policy = result[1]
-                opt_dist = result[2]
-                expected_returns = result[3]
-
-                D[tree_policy] = Dict("reward_seed" => reward_seed, "mf_seed_list" => mf_seed_list, "mcts_seed_list" => mcts_seed_list, "N" => N, "nx" => nx, "ny" => ny, "opt_dist" => opt_dist, "expected_returns" => expected_returns)
+            for tree_policy in tree_policies
+                opt_dist, expected_returns = runExp(scenario, mf_seed_list, mcts_seed_list, tree_policy, N, nx = nx, ny = ny, nloop_max = 1000000, nloop_min = 100)
+                D[(scenario, tree_policy)] = Dict("mf_seed_list" => mf_seed_list, "mcts_seed_list" => mcts_seed_list, "N" => N, "nx" => nx, "ny" => ny, "opt_dist" => opt_dist, "expected_returns" => expected_returns)
             end
 
         end
 
-    else
-        for tree_policy in tree_policies
-            opt_dist, expected_returns = runExp(reward_seed, mf_seed_list, mcts_seed_list, tree_policy, N, nx = nx, ny = ny, nloop_max = 1000000, nloop_min = 100)
-            D[tree_policy] = Dict("reward_seed" => reward_seed, "mf_seed_list" => mf_seed_list, "mcts_seed_list" => mcts_seed_list, "N" => N, "nx" => nx, "ny" => ny, "opt_dist" => opt_dist, "expected_returns" => expected_returns)
+        if isfile("exp.jld")
+            D_ = load("exp.jld", "DATA");
+
+            for (key, experiment) in D
+                scenario, tree_policy = key
+                D_[(scenario, tree_policy)] = experiment
+            end
+
+        else
+            D_ = D
+
         end
 
+        save("exp.jld", "DATA", D_)
     end
-
-    f = open("exp.json", "w")
-    JSON.print(f, D)
-    close(f)
 end
 
 
